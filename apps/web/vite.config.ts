68908d4287f1ada3fa747bf4d5b64941d7b9d452
Nicholai Nissen <nicholainissen@gmail.com>
2022-12-23 23:12:28 +0100

    fix: config issues

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -14,7 +14,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:31337'
+        target: 'http://localhost:31337',
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
