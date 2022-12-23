9da00a35306d1523c6bd180620160ce3618780fc
Nicholai Nissen <nicholainissen@gmail.com>
2022-12-23 23:47:59 +0100

    feat: add cache handling

--- apps/api/src/gpt/controller.ts
+++ apps/api/src/gpt/controller.ts
@@ -2,6 +2,7 @@ import { Controller, accept } from '@curveball/controller'
 import { BadRequest } from '@curveball/http-errors/dist'
 import type { Context } from '@curveball/core'
 import { isNil } from 'ramda'
+import { setTimeout } from 'node:timers/promises'
 
 type HashFn = (input: string) => string
 interface Cache {
@@ -18,12 +19,32 @@ export class GptController extends Controller {
   }
 
   @accept('application/json')
-  post (ctx: Context<{ query?: string }>): void {
-    if (isNil(ctx.request.body.query)) throw new BadRequest('Field \'query\' is missing') // eslint-disable-line
+  async post (ctx: Context<{ query?: string }>): Promise<void> {
+    const { query } = ctx.request.body
+    if (isNil(query)) throw new BadRequest('Field \'query\' is missing') // eslint-disable-line
+    const hash = this.hash(query)
 
     ctx.response.type = 'application/json'
     ctx.response.body = {
-      message: 'Hello Gumse'
+      message: await this.cachedQuery(hash, this.queryGpt.bind(this, query))
     }
+  }
+
+  async queryGpt (query: string): Promise<string> {
+    await setTimeout(666) /* very expensive api request */
+    return 'Hello Gumse: ' + query
+  }
+
+  async cachedQuery (hash: string, query: () => Promise<string>): Promise<string> {
+    const match = this.cache.get(hash)
+    if (process.env['NODE_ENV'] === 'development') {
+      console.log(`Cache ${isNil(match) ? 'miss' : 'hit'} for ${hash.substring(0, 10)}`)
+    }
+
+    const data = match ?? await query()
+
+    if (isNil(match)) this.cache.set(hash, data)
+
+    return data
   }
 }
