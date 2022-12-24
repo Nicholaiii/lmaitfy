2695e957a2383bc0d1bb7e4af2d291024ac7d093
Nicholai Nissen <nicholainissen@gmail.com>
2022-12-24 15:07:16 +0100

    feat: configure cors

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -2,6 +2,7 @@ import { Application } from '@curveball/core'
 import bodyParser from '@curveball/bodyparser'
 import router from '@curveball/router'
 import problemMw from '@curveball/problem'
+import cors from '@curveball/cors'
 
 import { GptController } from './gpt/controller.js'
 
@@ -10,6 +11,11 @@ export function start (port: number, GptDeps: ConstructorParameters<typeof GptCo
 
   app.use(bodyParser())
   app.use(problemMw())
+  app.use(cors({
+    allowOrigin: ['https://lmaitfy.onrender.com', 'https://lmaitfy.com'],
+    allowHeaders: ['Content-Type', 'Accept'],
+    allowMethods: ['GET', 'POST']
+  }))
 
   app.use(router('/gpt', new GptController(...GptDeps)))
 
