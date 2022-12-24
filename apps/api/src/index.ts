aebde2e9889a1e99cccf099e63ee3280a28ff64a
Nicholai Nissen <nicholainissen@gmail.com>
2022-12-24 15:12:47 +0100

    feat: use PORT from env

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,12 +3,12 @@ import * as app from './app.js'
 import { sha256 } from './hash.js'
 import { lru } from './lru-cache.js'
 
-function main (port: number = 31337): void {
+function main (port: number): void {
   console.log(`(っ◔◡◔)っ ♥ lmaitfy ♥ v${require('../package.json').version as string}`)
   app.start(port, [sha256, lru])
   console.log(`Listening on port ${port}`)
 }
 
 if (process.env['NODE_ENV'] !== 'test') {
-  main()
+  main(Number(process.env['PORT']) ?? 31337)
 }
